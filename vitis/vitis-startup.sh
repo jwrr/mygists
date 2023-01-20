139ee0337a1c4a36f7b007b2c9b972ad2b493b65
jwrr <jwrr.com@gmail.com>
2023-01-20 18:42:06 -0500

    Add vitis-startup.sh

---
+++ vitis/vitis-startup.sh
@@ -0,0 +1,17 @@
+export XILINX_VERSION=2022.2
+export XILINX_PATH=/tools/Xilinx
+echo source $XILINX_PATH/Vitis/$XILINX_VERSION/bin/setupEnv.sh
+source $XILINX_PATH/Vitis/$XILINX_VERSION/bin/setupEnv.sh
+echo source $XILINX_PATH/Vitis/$XILINX_VERSION/settings64.sh
+source $XILINX_PATH/Vitis/$XILINX_VERSION/settings64.sh
+
+## PetaLinux requires Bash shell.
+## Use this to set shell to bash. Select 'NO' to switch to Bash.
+# sudo dpkg-reconfigure dash
+
+echo sudo service tftpd-hpa restart
+sudo service tftpd-hpa restart
+echo source $XILINX_PATH/PetaLinux/$XILINX_VERSION/settings.sh
+source $XILINX_PATH/PetaLinux/$XILINX_VERSION/settings.sh
+echo done
+
